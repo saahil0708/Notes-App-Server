4c8710afbb001d5eb7e9c8097b2adb34180e5c53
saahil0708 <saahil.ind.dgp@gmail.com>
2025-07-13 17:50:13 +0530

    Change

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
 
-import DB from './Config/Db.js';
-import NotesRoutes from './Routes/Notes.Route.js';
-import UserRoutes from './Routes/User.Route.js';
+import DB from "./Config/Db.js";
+import NotesRoutes from "./Routes/Notes.Route.js";
+import UserRoutes from "./Routes/User.Route.js";
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true // if you're using cookies or auth headers
-  }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://notes-app-two-plum.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // if you're using cookies or auth headers
+  })
+);
 
 DB();
 
@@ -20,13 +23,13 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Notes App API');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Notes App API");
 });
 
-app.use('/notes', NotesRoutes);
-app.use('/users', UserRoutes);
+app.use("/notes", NotesRoutes);
+app.use("/users", UserRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is Running on Port: ${PORT}`);  
+  console.log(`Server is Running on Port: ${PORT}`);
 });
